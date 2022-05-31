0bacf11f04e30645ee90a2d7669ab4a7e0a98298
vinzphenix <vincent.degrooff@student.uclouvain.be>
2022-05-31 02:18:52 +0200

    improve duffing

--- Duffing_oscillator/README.md
+++ Duffing_oscillator/README.md
@@ -5,4 +5,13 @@
 
 ## Poincaré section
 ![example](./poincare.gif)
-`The animation modifies the section progressively`
+
+The poincare section is progressively modified during the animation.
+
+## Commands to make gif from images
+- png to mp4 to gif
+  - ```ffmpeg -framerate 30 -i frame_%05d.png -vcodec libx264 -crf 15 -r 30 -pix_fmt yuv420p out.mp4```
+  - ```ffmpeg -ss 0.0 -i out.mp4 -f gif out.gif```
+- png to gif using a palette
+  - ```ffmpeg -i frame_%05d.png -vf palettegen=reserve_transparent=1 palette.png```
+  - ```ffmpeg -framerate 30 -i frame_%05d.png -i palette.png -lavfi paletteuse=alpha_threshold=128 -gifflags -offsetting out.gif```
--- Duffing_oscillator/poincare_duffing_animation.py
+++ Duffing_oscillator/poincare_duffing_animation.py
@@ -1,14 +1,22 @@
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
-matplotlib.rcParams['mathtext.fontset'] = 'cm'
-matplotlib.rcParams['mathtext.rm'] = 'serif'
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['mathtext.rm'] = 'serif'
+
+# plt.rcParams['text.usetex'] = True
+plt.rcParams['mathtext.fontset'] = 'cm'
+
+ftSz1, ftSz2, ftSz3 = 20, 12, 10
+
 
 if __name__ == "__main__":
 
-    filename = "samples_3.txt"
+    sim_nb = 1
+    show_axis = True
+    filename = f"samples_{sim_nb:d}.txt"
     file = open(filename, 'r')
 
     line = next(file)
@@ -23,30 +31,41 @@ if __name__ == "__main__":
     L_x, L_y = x_lim[1] - x_lim[0], y_lim[1] - y_lim[0]
     m_x, m_y = (x_lim[1] + x_lim[0]) / 2, (y_lim[1] + y_lim[0]) / 2
 
-    for i in range(nFigs):
-        print("figure {:d}".format(i))
+    fig, ax = plt.subplots(1, 1, figsize=(8., 5.), constrained_layout=False)
 
-        fig, ax = plt.subplots(1, 1, figsize=(8, 5), constrained_layout=False)
-        ax.set_xlabel(r'$x$', size=12)
-        ax.set_ylabel(r'$v$', rotation=0, size=12)
-        ax.set_xlim([-1.8, 1.8])
-        ax.set_ylim([-3.5, 3.5])
-        ax.tick_params(axis='both', which='major', labelsize=8)
+    # ax.axis([-1., 1., -3.5, 3.5])
+    ax.axis([-1.6, 1.6, -1.2, 1.2])
+    # ax.axis([-1.6, 1.6, -2.2, 2.2])
 
+    if show_axis:
+        ax.set_xlabel(r'$x$', size=ftSz2)
+        ax.set_ylabel(r'$\dot x$', rotation=0, size=ftSz2)
         ax.set_title(r"$\ddot{x} + \gamma \dot{x} + \alpha x + \beta x^3 \,=\, A \cos{(\omega \, t)}$")
-        ax.text(0.025, 0.95, r'$\alpha = {:.2f} $'.format(alpha), fontsize=10, wrap=True, transform=ax.transAxes)
-        ax.text(0.025, 0.90, r'$\beta  = {:.2f} $'.format(beta), fontsize=10, wrap=True, transform=ax.transAxes)
-        ax.text(0.025, 0.85, r'$\gamma = {:.2f} $'.format(gamma), fontsize=10, wrap=True, transform=ax.transAxes)
-        ax.text(0.150, 0.95, r'$A = {:.2f} $'.format(A), fontsize=10, wrap=True, transform=ax.transAxes)
-        ax.text(0.150, 0.90, r'$\omega = {:.2f} $'.format(w), fontsize=10, wrap=True, transform=ax.transAxes)
+        ax.tick_params(axis='both', which='major', labelsize=8)
+        ax.text(0.025, 0.95, r'$\alpha = {:.2f} $'.format(alpha), fontsize=ftSz3, transform=ax.transAxes)
+        ax.text(0.025, 0.90, r'$\beta  = {:.2f} $'.format(beta), fontsize=ftSz3, transform=ax.transAxes)
+        ax.text(0.025, 0.85, r'$\gamma = {:.2f} $'.format(gamma), fontsize=ftSz3, transform=ax.transAxes)
+        ax.text(0.150, 0.95, r'$A = {:.2f} $'.format(A), fontsize=ftSz3, transform=ax.transAxes)
+        ax.text(0.150, 0.90, r'$\omega = {:.2f} $'.format(w), fontsize=ftSz3, transform=ax.transAxes)
+    else:
+        ax.axis("off")
 
-        ax.text(0.785, 0.895, r"$\varphi = {:.3f} \;\pi$".format(2 * i / nFigs), fontsize=12, wrap=True, transform=ax.transAxes)
+    phi_template = r"$\varphi = {:.2f} \;\pi$" if show_axis else None
+    phi_text = ax.text(0.86, 0.91, "", fontsize=ftSz2, transform=ax.transAxes) if show_axis else None
+
+    dots, = ax.plot([], [], 'o', markersize=0.5, color='black')
+    # fig.subplots_adjust(bottom=0.1, top=0.9, left=0.075, right=0.95)
+    fig.tight_layout()
+
+    for i in tqdm(range(nFigs)):
+
+        if show_axis:
+            phi_text.set_text(phi_template.format(2 * i / nFigs))
 
         df_selected = df[df["fig"] == i]
         line_x = np.array(df_selected["x"])
         line_v = np.array(df_selected["v"])
+        dots.set_data(line_x, line_v)
 
-        ax.plot(line_x, line_v, 'o', markersize=0.5, color='black')
-        fig.subplots_adjust(bottom=0.1, top=0.9, left=0.075, right=0.95)
-        fig.savefig("./Duffing_3/frame_{:04d}".format(i))
-        plt.close()
+        str_additional = "" if show_axis else "_no_axis"
+        fig.savefig(f"./frames/case_{sim_nb:d}{str_additional:s}/frame_{i:05d}", transparent=not show_axis)
